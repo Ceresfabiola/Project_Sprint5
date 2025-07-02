86a76751b8ea08080656f4e9dd0223d4ad0ed2a1
Ceres <217501620+Ceresfabiola@users.noreply.github.com>
2025-07-01 21:40:32 -0500

    Add new files and folders for Sprint 5 project

---
+++ app.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+# Creating the header
+st.header('Vehicles Dashboard')
+
+# Reading the data
+car_data = pd.read_csv('../vehicles.csv')
+
+# Creating the button
+hist_button = st.button('Create Histogram')
+
+if hist_button:  # if the button is clicked
+    # display a message
+    st.write('Creating a histogram for the car sales ads dataset')
+
+    # create a histogram
+    fig = px.histogram(car_data, x="odometer")
+
+    # display an interactive Plotly chart
+    st.plotly_chart(fig, use_container_width=True)
+
+    # Creating the 2nd button
+scatter_button = st.button('Create Scatter Plot')
+
+if scatter_button:  # if the button is clicked
+    # display a message
+    st.write('Creating a scatter plot for the car sales ads dataset')
+
+    # create a scatter plot
+    fig = px.scatter(car_data, x="odometer", y='price')
+
+    # display an interactive Plotly chart
+    st.plotly_chart(fig, use_container_width=True)
